07a1eeceb15de8232141ad237c90181f603302e7
Eric O. Salalima <eosalalima@gmail.com>
2025-09-14 01:05:51 +0800

    fix: use generated Prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,6 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+// Use the generated Prisma client located in lib/generated/prisma
+import { PrismaClient } from './generated/prisma'
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
