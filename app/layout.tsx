7feed6ee07a956026733dc9fc12ae4e75570d701
Eric O. Salalima <eosalalima@gmail.com>
2025-09-18 04:30:52 +0000

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
     const { userId } = authState;
 
     return (
-        <ClerkProvider initialState={authState}>
+        <ClerkProvider>
             <html lang="en">
                 <body
                     className={`${geistSans.variable} ${geistMono.variable} antialiased`}
